86020a1374a4751f56a77d1a95793e1c0ed42c4b
iamnuked <qjawls6262@gmail.com>
2026-03-23 19:44:55 +0900

    0323 문제풀이

--- week4/problem-solving/난이도중_BFS_동전2_골드5.py
+++ week4/problem-solving/난이도중_BFS_동전2_골드5.py
@@ -1,2 +1,3 @@
 # BFS - 동전 2 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2294
+
--- week4/problem-solving/난이도중_그래프DFSBFS_DFS와BFS_실버2.py
+++ week4/problem-solving/난이도중_그래프DFSBFS_DFS와BFS_실버2.py
@@ -1,2 +1,84 @@
 # 그래프, DFS, BFS - DFS와 BFS (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1260
+
+
+'''
+작은 숫자부터 방문
+
+'''
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+
+# def dfs(start):
+#     stack = []
+#     stack.append(start)
+#     order = []
+
+#     while stack:
+#         temp_stack = []
+#         v = stack.pop()
+#         visited[v] = True
+#         order.append(v)
+#         for e in graph[v]:
+#             if visited[e] == False:
+#                 temp_stack.append(e)
+#                 visited[e] = True
+#         temp_stack.sort(reverse=True)
+#         stack.extend(temp_stack)
+
+#     return order
+
+def dfs(start, visited):
+
+    visited[start] = True
+    order.append(start)
+    sorted_edges = sorted(graph[start])
+    for e in sorted_edges:
+        if visited[e] == False:
+            dfs(e, visited)
+
+    return order
+    
+
+
+
+
+def bfs(start):
+    q = deque()
+    q.append(start)
+    order = []
+    
+    while q:
+        temp_q = deque()
+        v = q.popleft()
+        visited[v] = True
+        order.append(v)
+        for e in graph[v]:
+            if visited[e] == False:
+                temp_q.append(e)
+                visited[e] = True
+        temp_q = sorted(temp_q)
+        q.extend(temp_q)
+    return order
+
+
+
+N, M, start = map(int, input().split())
+
+# 그래프 만들기
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+order = []
+visited = [False] * (N+1)
+print(' '.join(map(str, dfs(start, visited))))
+
+visited = [False] * (N+1)
+print(' '.join(map(str, bfs(start))))
