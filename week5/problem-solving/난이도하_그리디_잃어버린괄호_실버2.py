9182f0be078274755f6d8ed1b534d108ef210613
iamnuked <qjawls6262@gmail.com>
2026-03-27 17:15:47 +0900

    0327 문제풀이

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,26 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+'''
+55-50+40+20-50+10 -> 최소 55-(50+40+20)-(50+10)
+
+-로 스플릿
++로 스플릿 후 sum
+첫번째 - sum1 -sum2 ...
+
+'''
+
+# 완
+
+split_by_minus = input().split('-')
+
+sums = []
+for x in split_by_minus:
+    sums.append(sum(map(int, x.split('+'))))
+
+result = sums[0]
+for i in range(1, len(sums)):
+    result -= sums[i]
+
+print(result)
+
