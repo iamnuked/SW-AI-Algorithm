76ba15eaf8e9eb2cf72efd738d8ac2e504f2fc59
iamnuked <qjawls6262@gmail.com>
2026-03-27 17:51:32 +0900

    0327 문제풀이

--- week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -8,34 +8,68 @@ N = 3 -> 3 (+1)
 N = 4 -> 5 (+2)
 N = 5 -> 8 (+3)
 N = 6 -> 13 (+5)
+N = 7 -> 21 (+13)
 
+??? 피보나치네
 
 '''
 
-import sys
-sys.setrecursionlimit(10**6)
+# 완
 
-def comb(n, r):
-    result = 1
-    for i in range(r):
-        result *= (n-i)
-        result //= (i+1)
-    return result
+N = int(input()) - 1
+
+fibo = [1, 2, 3]
+
+
+for i in range(N):
+    fibo[i%3] = (fibo[(i+1)%3] + fibo[(i+2)%3]) % 15746 # 아주 큰 숫자를 리스트에 계속 대입할 경우 시간 비효율 발생
+
+print(fibo[N%3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 시간 초과
+
+# import sys
+# sys.setrecursionlimit(10**6)
+
+# def comb(n, r):
+#     result = 1
+#     for i in range(r):
+#         result *= (n-i)
+#         result //= (i+1)
+#     return result
     
 
 
 
-N = int(input())
+# N = int(input())
 
-def f(N):
-    count = 0
-    r = 0
-    while N >= r:
-        count += comb(N, r)
-        N -= 1
-        r += 1
-    return count
+# def f(N):
+#     count = 0
+#     r = 0
+#     while N >= r:
+#         count += comb(N, r)
+#         N -= 1
+#         r += 1
+#     return count
 
 
 
-print(f(N)%15746)
+# print(f(N)%15746)
