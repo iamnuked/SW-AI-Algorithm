113eeeff651f08a10bdc73b5dfa41d8aae6dcd2c
iamnuked <qjawls6262@gmail.com>
2026-03-30 17:30:15 +0900

    0330 문제풀이

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,21 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+
+
+str1 = input()
+str2 = input()
+
+
+lcs = [ [0] * (len(str1) + 1) for _ in range(len(str2) + 1) ]
+
+for y in range(1, len(str2)+1):
+    for x in range(1, len(str1)+1):
+        if str1[x-1] == str2[y-1]:
+            lcs[y][x] = lcs[y][x-1] + 1
+            break
+        else:
+            lcs[y][x] = lcs[y][x-1]
+
+
+print(lcs)
+    
