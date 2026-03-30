73527a94e30b53e6d347d7050c06e5cbb7114c7d
iamnuked <qjawls6262@gmail.com>
2026-03-30 16:37:17 +0900

    0330 문제풀이

--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -6,26 +6,47 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
+# 복습 필요
+
+
+'''
+
+|  i  \  m  |  0 |  1 |  2 |  3 |  4 |
+| --------- | -- | -- | -- | -- | -- |
+| 0 (1원만)  |  1 |  1 |  1 |  1 |  1 |
+| 1 (1원,2원)|  1 |  1 |  2 |  2 |  3 |
+
+
+|     i \ m    |  0 |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 | 10 |
+| ------------ | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- | -- |
+| 0 (2)        |  1 |  0 |  1 |  0 |  1 |  0 |  1 |  0 |  1 |  0 |  1 |
+| 1 (2, 3)     |  1 |  0 |  1 |  1 |  1 |  1 |  2 |  1 |  2 |  2 |  2 |
+| 2 (2, 3, 5,) |  1 |  0 |  1 |  1 |  1 |  2 |  2 |  2 |  3 |  3 |  4 |
+
+
+
+'''
+
 def coin_dp(coins, M, memo=None):
     if memo == None:
-        memo = [0] * (M+len(coins))
-        for i, v in enumerate(coins):
-            memo[i] = v
+        memo = [0] * (M+1)
+        memo[0] = 1
     
-    if memo[M] != 0:
-        return memo[M]
-    
-    for i in coins:
-        memo[M] += coin_dp(coins, M-i, memo)
-    
+    for c in coins:
+        for i in range(c, M+1):
+            memo[i] += memo[i-c]
     return memo[M]
 
 
-T = int(input())
+
+
+T = int(input()) # T: 테스트 케이스  /  N: 동전 가지 수  /  M: 금액
 for _ in range(T):
     N = int(input())
     coins = list(map(int, input().split()))
     M = int(input())
+
+
     print(coin_dp(coins, M))
     
     
